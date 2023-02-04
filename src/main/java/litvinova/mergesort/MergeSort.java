f5dd88b43e6d446943e1145488ea9f0f35d8ba3c
daryalitvinova <93474867+daryalitvinova@users.noreply.github.com>
2023-02-04 21:45:23 +0700

    done

--- src/main/java/litvinova/mergesort/MergeSort.java
+++ src/main/java/litvinova/mergesort/MergeSort.java
@@ -4,8 +4,8 @@ public class MergeSort {
     public static void main(String[] args) throws NoRequiredParamException {
         int k = 0;
         int sortMode = 0;   // 0 - по возрастанию, 1 - по убыванию
-        if(args.length == 0) {
-            throw new NoRequiredParamException("-i : сортировка чисел, -s : сортировка строк\n\t" +
+        if(args.length < 3) {
+            throw new NoRequiredParamException("-i : сортировка целых чисел, -s : сортировка строк\n\t" +
                     "Для выполнения сортировки необходим выходной файл и как минимум один входной");
         }
         if (args[k].equals("-d")) {
@@ -17,12 +17,14 @@ public class MergeSort {
         if (args[k].equals("-i")) {
             k++;
             MergeSortInt.Sort(args, k, sortMode);
+            System.out.println("Сортировка завершена");
         } else if (args[k].equals("-s")) {
             k++;
             MergeSortString.Sort(args, k, sortMode);
+            System.out.println("Сортировка завершена");
 
         } else {
-            throw new NoRequiredParamException("-i : сортировка чисел, -s : сортировка строк");
+            throw new NoRequiredParamException("-i : сортировка целых чисел, -s : сортировка строк");
         }
     }
 }
--- src/main/java/litvinova/mergesort/NoRequiredParamException.java
+++ src/main/java/litvinova/mergesort/NoRequiredParamException.java
@@ -2,6 +2,6 @@ package litvinova.mergesort;
 
 public class NoRequiredParamException extends Exception{
     public NoRequiredParamException(String string) {
-        System.err.println("Отсутствует или недопустимый обязательный параметр\n\t" + string);
+        System.err.println("Отсутствует или недопустимый параметр\n\t" + string);
     }
 }
